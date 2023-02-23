f2bbef5e34d8b663e1282d45aeda138037a5f2a0
mfont <ithebossx@gmail.com>
2023-02-23 17:16:09 +0100

    sitemap formatted

--- pages/sitemap/index.tsx
+++ pages/sitemap/index.tsx
@@ -5,22 +5,22 @@ import { GetStaticProps } from 'next';
 import GlobalStore from '@/stores/GlobalStore';
 
 interface ISitemapProps {
-  content: string;
+	content: string;
 }
 
 const Sitemap: React.FC<ISitemapProps> = ({ content }) => (
-  <>
-    <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }} />
-  </>
+	<>
+		<div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }} />
+	</>
 );
 
 export const getStaticProps: GetStaticProps<ISitemapProps> = async () => {
-  const sitemapHTML: ISitemap = await GlobalStore.getSitemap();
-  const content = sitemapHTML.content;
-  return {
-    props: { content },
-    revalidate: 1800 // regenerate every 30 minutes (1800 seconds)
-  };
+	const sitemapHTML: ISitemap = await GlobalStore.getSitemap();
+	const content = sitemapHTML.content;
+	return {
+		props: { content },
+		revalidate: 1800 // regenerate every 30 minutes (1800 seconds)
+	};
 };
 
 export default Sitemap;
